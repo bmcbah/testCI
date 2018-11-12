355ac98116be0560b14058347bdddab537b6c5e8
bmcbah <bahcelloum@gmail.com>
2018-11-12 18:24:22 +0100

    Initial commit 1

--- src/AppBundle/Entity/Product.php
+++ src/AppBundle/Entity/Product.php
@@ -31,4 +31,17 @@ class Product
 
         return $this->price * 0.196;
     }
+
+    public function computeTVA1()
+        {
+            if ($this->price < 0) {
+                throw new \LogicException('The TVA cannot be negative.');
+            }
+
+            if (self::FOOD_PRODUCT == $this->type) {
+                return $this->price * 0.055;
+            }
+
+            return $this->price * 0.196;
+        }
 }
